48172e9433146d8bc247d9b904ce987185e53f99
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-12-27 20:08:04 +0000

    Move semantics to diferent folder

--- tox/__init__.py
+++ tox/__init__.py
@@ -1,9 +1,9 @@
 from tox.utils.colors import *
 from tox.utils.errors import compiler_warning, compiler_error, compiler_note, syntax_error, find_column, std_message, lex_error
-from tox.parsing._scopes import Scope, MetaData
-from tox.parsing._type_check import TypeCheck
-from tox.parsing._functions import Functions, FunctionData
-from tox.parsing._expression import (
+from tox.semantics._scopes import Scope, MetaData
+from tox.semantics._type_check import TypeCheck
+from tox.semantics._functions import Functions, FunctionData
+from tox.semantics._expression import (
     Primary,
     Unary,
     Factor,
@@ -13,7 +13,7 @@ from tox.parsing._expression import (
     SubExpression,
     Expression,
 )
-from tox.parsing._statement import (
+from tox.semantics._statement import (
     Print,
     Assignment,
     Declaration,
