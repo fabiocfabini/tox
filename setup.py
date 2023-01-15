55a137f3aaf32e8ccc9b3ea235b42d495cedde69
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-01-15 15:37:10 +0000

    added authors

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="lat",
     version="0.1",
-    author="Fábio Carneiro",
-    author_email="fabiolucas.carneiro@gmail.com",
+    author="Fábio Carneiro, Gabriela Jordão, Miguel Caçador Peixoto",
+    author_email="fabiolucas.carneiro@gmail.com, mgabijo@gmail.com, miguelpeixoto457@gmail.com",
     description="Lat: a ply programming language",
     packages=find_packages(),
     install_requires=["ply", "tqdm"],
